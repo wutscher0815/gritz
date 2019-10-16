d0db7475004c313d9a10a8ebee9867048209661c
Hartwig Wutscher <hartwig.wutscher@at.ibm.com>
2019-10-16 09:58:34 +0200

    prod build

---
+++ docs/precache-manifest.42b3d87c6bad0aa711cffd930692ea99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f33a1fe44852a3f62632e294932a4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d21563e5a5ee021c8327",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9a3771b9b89e4ba4ff9b",
+    "url": "/static/css/main.cee45233.chunk.css"
+  },
+  {
+    "revision": "d21563e5a5ee021c8327",
+    "url": "/static/js/2.459572f5.chunk.js"
+  },
+  {
+    "revision": "9a3771b9b89e4ba4ff9b",
+    "url": "/static/js/main.44239faf.chunk.js"
+  },
+  {
+    "revision": "2c206573e040dccc4ce9",
+    "url": "/static/js/runtime~main.251f6db8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
